09e0146edbaad268f27dd5bd77433de892225368
Thomas <thomascode92@hotmail.com>
2025-07-11 08:26:59 +0200

    Add a Subscriber

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -10,6 +10,14 @@ const client = createClient({
 client.on("error", err => console.error("Redis Client Error", err));
 client.connect().then(() => console.log("Redis connected successfully!"));
 
+const sub = client.duplicate();
+sub.on("error", err => console.error("Redis Subscriber Error", err));
+await sub.connect();
+
+const listener = (message: string, channel: string) =>
+  console.log(message, channel);
+await sub.subscribe("message", listener);
+
 // Test Redis connection
 await client.set("hello", "world");
 const value = await client.get("hello");
