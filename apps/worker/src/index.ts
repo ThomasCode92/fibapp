719955400b6c62120adbc069eff8399e21117eaa
Thomas <thomascode92@hotmail.com>
2025-07-11 08:14:19 +0200

    Implement a Redis Client

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, from the worker app!");
+import { createClient } from "@redis/client";
+
+import * as env from "~/config/env";
+
+// Redis client setup
+const client = createClient({
+  url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`,
+});
+
+client.on("error", err => console.error("Redis Client Error", err));
+client.connect().then(() => console.log("Redis connected successfully!"));
+
+// Test Redis connection
+await client.set("hello", "world");
+const value = await client.get("hello");
+console.log("Hello", value);
