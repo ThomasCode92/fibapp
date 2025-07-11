cf62eabe8dd9a9ff80709f5b4ac04e15de15f966
Thomas <thomascode92@hotmail.com>
2025-07-11 08:36:41 +0200

    Calculate and Store Fibonnaci Numbers

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@redis/client";
 
 import * as env from "~/config/env";
+import { fib } from "~/utils/fib";
 
 // Redis client setup
 const client = createClient({
@@ -14,11 +15,12 @@ const sub = client.duplicate();
 sub.on("error", err => console.error("Redis Subscriber Error", err));
 await sub.connect();
 
-const listener = (message: string, channel: string) =>
-  console.log(message, channel);
-await sub.subscribe("message", listener);
+const listener = async (message: string, channel: string) => {
+  console.log(`Received message "${message}" from channel "${channel}"`);
+  const fibNum = fib(Number(message));
+  await client.set(`fib:${message}`, fibNum);
+  const result = await client.get(`fib:${message}`);
+  console.log(`Fibonacci of ${message} is ${result}`);
+};
 
-// Test Redis connection
-await client.set("hello", "world");
-const value = await client.get("hello");
-console.log("Hello", value);
+await sub.subscribe("message", listener);
