7a8fead8d5b30d94b34505ca9f825312b57bdd92
Thomas <thomascode92@hotmail.com>
2025-09-04 08:29:22 +0200

    Add Tests

--- apps/api/src/utils/schema.utils.ts
+++ apps/api/src/utils/schema.utils.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const indexSchema = z.object({
-  index: z.coerce.number().max(40),
+  index: z.coerce.number().min(0).max(40),
 });
 
 export type IndexSchema = z.infer<typeof indexSchema>;
--- apps/api/test/values.test.ts
+++ apps/api/test/values.test.ts
@@ -20,3 +20,36 @@ describe("GET /values/calculated", function () {
     expect(body.data).toEqual({});
   });
 });
+
+describe("POST /values", function () {
+  const publisher = { connect: vi.fn(), publish: vi.fn() };
+
+  // Mock redis client methods
+  // NOTE: can be replaced with a test docker setup
+  vi.spyOn(redis.redisClient, "hSet").mockResolvedValue(0);
+  vi.spyOn(redis.redisClient, "duplicate").mockReturnValue(
+    publisher as unknown as ReturnType<typeof redis.redisClient.duplicate>,
+  );
+
+  test("should respond with correct content and status code", async function () {
+    const { body } = await supertest(app)
+      .post("/api/values")
+      .send({ index: 10 })
+      .expect(201);
+    expect(body.message).toBe("data stored successfully");
+  });
+
+  test("should store the correct value in the redis cache", async function () {
+    await supertest(app).post("/api/values").send({ index: 10 });
+    expect(redis.redisClient.hSet).toHaveBeenCalledWith(`values`, `fib:10`, "");
+    expect(publisher.publish).toHaveBeenCalledWith("message", "10");
+  });
+
+  test("should respond with correct content and status if body is invalid", async function () {
+    const { body } = await supertest(app)
+      .post("/api/values")
+      .send({ index: -10 })
+      .expect(422);
+    expect(body.message).toBe("validation error");
+  });
+});
