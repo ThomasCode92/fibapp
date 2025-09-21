8d93b064b5dc6d3856cb457176dc1f841bbd4e8c
Thomas <thomascode92@hotmail.com>
2025-09-21 19:13:12 +0200

    Install and Configure Tanstack Query

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "./App.css";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <main className="container">
-      <hgroup>
-        <h1>FibApp – A Fibonacci Calculator</h1>
-        <p>Find out the value of your favorite Fibonacci number!</p>
-      </hgroup>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="container">
+        <hgroup>
+          <h1>FibApp – A Fibonacci Calculator</h1>
+          <p>Find out the value of your favorite Fibonacci number!</p>
+        </hgroup>
+      </main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
